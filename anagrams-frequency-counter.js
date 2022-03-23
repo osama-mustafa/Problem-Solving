b0b268c19b271b65b5561b8c39f05aea21ac80eb
osama-mustafa <semsem6661@gmail.com>
2022-03-23 20:08:31 +0200

    Add Anagram Problem Using Frequency Counter Pattern

---
+++ anagrams-frequency-counter.js
@@ -0,0 +1,28 @@
+function isAnagram(string1, string2) {
+
+    if (string1.length !== string2.length) {
+        return false
+    }
+    
+    let lowercasedString1 = string1.toLowerCase();
+    let lowercasedString2 = string2.toLowerCase();
+
+    let lookup = {}
+    for (firstValue of lowercasedString1) {
+        lookup[firstValue] = (lookup[firstValue] || 0) + 1
+    }
+
+    for (secondValue of lowercasedString2) {
+        if (! lookup[secondValue]) {
+            return false
+        }
+        lookup[secondValue] -= 1
+    }
+
+    return true
+}
+
+console.log(isAnagram('race', 'CARE')) // true
+console.log(isAnagram('part', 'trap')) // true
+console.log(isAnagram('heart', 'earth')) // true
+console.log(isAnagram('knee', 'keen'))  // true
