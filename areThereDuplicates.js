7142291f8af32d2398f55eb139d34a9c6b2e49d0
osama-mustafa <semsem6661@gmail.com>
2022-03-26 15:46:38 +0200

    Add areThereDuplicates Problem

---
+++ areThereDuplicates.js
@@ -0,0 +1,28 @@
+
+/***
+    Implement a function called areThereDuplicates,
+    which accepts a variable number of argumants and checks whether
+    there are any duplicates among the arguments passed in.
+
+    ====================
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    ======================
+***/
+
+
+function areThereDuplicates(...args) {
+
+    let lookup = {}
+    for (let value of args) {
+        lookup[value] = (lookup[value] || 0) + 1
+        if (lookup[value] > 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(areThereDuplicates(1, 2, 3))
+console.log(areThereDuplicates(1, 2, 2))
+console.log(areThereDuplicates('a', 'b', 'c', 'a'))
