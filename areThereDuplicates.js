843e2039cfa6c650aed8b1b70fdfb18838f23f67
osama-mustafa <semsem6661@gmail.com>
2022-03-26 15:48:43 +0200

    Add areThereDuplicates Problem

--- areThereDuplicates.js
+++ areThereDuplicates.js
@@ -23,6 +23,6 @@ function areThereDuplicates(...args) {
     return false;
 }
 
-console.log(areThereDuplicates(1, 2, 3))
-console.log(areThereDuplicates(1, 2, 2))
-console.log(areThereDuplicates('a', 'b', 'c', 'a'))
+console.log(areThereDuplicates(1, 2, 3))            // false
+console.log(areThereDuplicates(1, 2, 2))            // true
+console.log(areThereDuplicates('a', 'b', 'c', 'a')) // true
